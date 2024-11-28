c696ea56b160c0fd127ad7f49a24a91ab802bcd8
Mikołaj <126266745+mczarkowski1@users.noreply.github.com>
2024-11-28 14:35:47 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -51,3 +51,19 @@ menuLinks.forEach((link) =>
     window.scrollTo({ top: section, behavior: 'smooth' });
   })
 );
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
