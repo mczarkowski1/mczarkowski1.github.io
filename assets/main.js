19681645de5711bd5c7590af12cfc76dc5614495
Mikołaj <126266745+mczarkowski1@users.noreply.github.com>
2024-11-29 00:56:24 +0100

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -58,7 +58,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("faq-active");
     var panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
       panel.style.maxHeight = null;
